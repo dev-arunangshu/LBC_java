1b296b53a95a57727110cff783f946f272bc114e
Arunangshu Das Sarma <arunangshu02.dev@gmail.com>
2026-04-19 20:53:00 +0530

    positive number

---
+++ PositiveNumber.java
@@ -0,0 +1,15 @@
+// finding if a number is positive
+import java.util.Scanner;
+public class PositiveNumber {
+   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the first number");
+    int num1=sc.nextInt();
+    System.out.println("Enter the second number");
+    int num2=sc.nextInt();
+
+    if (num1>=0){
+        System.out.println(num1+" is a positive number");
+    }
+   } 
+}
