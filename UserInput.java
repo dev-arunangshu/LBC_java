c00e0a0b838bfa1937c0d553972aef06e03896b7
Arunangshu Das Sarma <arunangshu02.dev@gmail.com>
2026-04-19 20:24:42 +0530

    user input

---
+++ UserInput.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class UserInput {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Please enter you name");
+        String name= sc.nextLine();
+        
+        System.out.println("Hello "+ name);
+
+    }
+}
